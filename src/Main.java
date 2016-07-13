bb826ddbc3e980e05a4a73d1ba213713f1e3d9da
Sathya Prabha Girish <sathya.prabha.girish@sap.com>
2016-07-13 15:04:44 -0400

    Generic Methods and accessing them

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,23 @@ public class Main {
 
         Box<Integer> simpleBox = new Box<>();
         simpleBox.set(new Integer(1));
-
         System.out.println(simpleBox.get());
+
+        Pair<Integer, String> p1 = new Pair<>(1, "apple");
+        Pair<Integer, String> p2 = new Pair<>(2, "pear");
+        Pair<Integer, String> p3 = new Pair<>(1, "apple");
+//        boolean same = Util.<Integer, String>compare(p1, p2);
+        boolean same = Util.compare(p1, p3);
+
+        if(same){
+            System.out.println("Same");
+
+        }
+
+        else{
+            System.out.println("not same");
+        }
+
+
     }
 }
